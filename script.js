71534e0f406ff01ff114a24f8a2752f90ed345d3
MelmPro <melm.pro@gmail.com>
2023-02-14 15:22:22 +0100

    Ajout de bouton via sricp.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const goTo = () => {
+    alert('vous avez appuyer sur un bouton');
+}
+
+const btn1 = document.getElementById('menuBtn1');
+btn1.addEventListener('click',goTo);
+
+const btn2 = document.getElementById('menuBtn2');
+btn2.addEventListener('click',goTo);
+
+const btn3 = document.getElementById('menuBtn3');
+btn3.addEventListener('click',goTo);
+
+const btn4 = document.getElementById('menuBtn4');
+btn4.addEventListener('click',goTo);
+
+const btn5 = document.getElementById('menuBtn5');
+btn5.addEventListener('click',goTo);
